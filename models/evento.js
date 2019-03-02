2d3fb18484afe51ed51d10a36a39eeb3936cc5db
Jorge R. Encinas <jorge.encinas@gmail.com>
2019-03-02 11:49:19 -0400

    refactor: Refactoring the code

--- models/evento.js
+++ models/evento.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose');
- 
-mongoose.connect('mongodb://user:password123@ds035573.mlab.com:35573/certificados');
+const mongoose = require('mongoose'),
+  USER = 'user',
+  PASS = 'password123',
+  HOST = 'ds035573.mlab.com',
+  PORT = '35573',
+  DATABASE_NAME = 'certificados',
+  URL = `mongodb://${USER}:${PASS}@${HOST}:${PORT}/${DATABASE_NAME}`,
+  Schema = mongoose.Schema,
+  // ObjectId = Schema.ObjectId,
+  Certificado = new Schema({
+    nombres: { type: String, required: true },
+    apellidos: { type: String, required: true },
+    tipo: { type: String, required: true },
+    evento: { type: String, required: true },
+    codigo: { type: String, required: true },
+    fecha: { type: String, required: true }
+  });
 
+mongoose.connect(URL, { useNewUrlParser: true });
 
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
- 
-const Certificado = new Schema({
-  nombres: {type: String, required: true},
-  apellidos: {type: String, required: true},
-  tipo: {type: String, required: true},
-  evento: {type: String, required: true},
-  codigo: {type: String, required: true},
-  fecha: {type: String, required: true}
-});
-
-
-module.exports.Certificado = mongoose.model('Certificado',Certificado)
+module.exports.Certificado = mongoose.model('Certificado', Certificado)
--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 
-var express=require('express');
-var bodyParser=require('body-parser');
-var app=express();
-var port=8085;
-var models=require('./models/evento');
-var id;
-app.use(bodyParser.urlencoded({extended: true}));
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express(),
+  port = 8085,
+  models = require('./models/evento');
+  //  id;
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/certificate',(req, res) => {
-  let body = req.body
-
-  let certificado = new models.Certificado({
-    nombres: body.nombres, 
-    apellidos: body.apellidos,
-    tipo: body.tipo,
-    evento: body.evento, 
-    codigo: body.codigo,
-    fecha: body.fecha 
-  })
+app.post('/certificate', (req, res) => {
+  let body = req.body,
+    certificado = new models.Certificado({
+      nombres: body.nombres,
+      apellidos: body.apellidos,
+      tipo: body.tipo,
+      evento: body.evento,
+      codigo: body.codigo,
+      fecha: body.fecha
+    })
 
   certificado.save((error, certificadoDB) => {
-    if ( error ) {
+    if (error) {
       res.status(500).json({
         ok: false,
         error
@@ -32,21 +31,21 @@ app.post('/certificate',(req, res) => {
       })
     }
   })
-  
+
 })
 
-app.get('/verify/:id',(req, res) => {
+app.get('/verify/:id', (req, res) => {
   let id = req.params.id
-  models.Certificado.findOne({ codigo : id }, 
+  models.Certificado.findOne({ codigo: id },
     function (err, result) {
-      if(err){
-        res.status(400).send({status: "error"});  
-      }else{
+      if (err) {
+        res.status(400).send({ status: "error" });
+      } else {
         res.json({
           status: "ok",
           result
-        })  
-      } 
+        })
+      }
     });
 })
 app.listen(port);
