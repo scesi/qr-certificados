b9646a3e1daa7a793be96cb88f396fff6bb43e72
Jorge R. Encinas <jorge.encinas@gmail.com>
2019-03-06 13:22:58 -0400

    add: new regex

--- public/validate.js
+++ public/validate.js
@@ -1,7 +1,13 @@
 const validar = ()=>{
-    const regex =  /.{8}/g
+    const regex =  /^[\w\d-_]{8}/g
     let code = document.getElementById("code").value;
     console.log(code)
     result = regex.exec(code)
+    let message = document.getElementById("message")
+    if(result != null)
+        message.innerHTML = "es codigo es valido"
+    else message.innerHTML = "codigo invalido"
     console.log(result)
+
+    return false
 }
